7f9685d1c0228a0a916761b249eb341064606433
mjones1818 <michael.jones0532@gmail.com>
2021-02-12 09:13:48 -0600

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 
-const MoviesPage = ({ match, movies }) => (
+const MoviesPage = ({ movies }) => (
   <div>
     <MoviesList movies={movies} />
   </div>
